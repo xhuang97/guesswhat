348df80c9e32e44eed6a278f4fbe039caa65520c
Yiwen Yuan <yiwenyuan@huntsman-ve702-5531.apng.wlan.private.upenn.edu>
2018-09-08 17:35:11 -0400

    front end stuff

--- videos/front-end/src/app/app.module.ts
+++ videos/front-end/src/app/app.module.ts
@@ -3,13 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-
+import { MaterialModule } from './material.module';
 import { AppComponent } from './app.component';
 import { BlogPostService } from './blog_post.service';
 import { UserService } from './user.service';
 import { ClassesComponent } from './classes/classes.component';
 import {MatDividerModule} from '@angular/material/divider';
-import { MaterialModule } from './material.module';
 import { HomeComponent } from './home/home.component';
 import { TeamComponent } from './team/team.component';
 
